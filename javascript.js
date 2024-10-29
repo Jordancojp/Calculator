1d2cffced70772ba1f1974400b6123e48e81a5eb
Jordancojp <117468147+Jordancojp@users.noreply.github.com>
2024-10-30 04:58:37 +0800

    blocking out javascript functions, changed the way buttons are generated

--- javascript.js
+++ javascript.js
@@ -1,20 +1,55 @@
 var topRowValues = ['/','x','-','+'];
-var numPadValues = ['7','8','9','4','5','6','1','2','3','0','.','CE'];
+var numPadValues = ['7','8','9','4','5','6','1','2','3','0'];
 var topRowElement = document.querySelector("#TopRow");
 var numPadElemenet = document.querySelector("#NumPad");
+
+var inputString = document.querySelector ("#InputAsString");
+var resultString = document.querySelector ("#UserResultCalc");
+
+var numX = '', numY = '';
+
 
 generateNumPad();
 
 function generateNumPad()
 {
     let iterator = 0;
-    topRowValues.forEach(value => createButtons(value, topRowElement));
-    numPadValues.forEach(value => createButtons(value, numPadElemenet));
+    topRowValues.forEach(value => createButtons(value, topRowElement, operatorPressed));
+    numPadValues.forEach(value => createButtons(value, numPadElemenet, numberPressed));
+    createButtons('.', numPadElemenet, decimalPressed);
+    createButtons('CE', numPadElemenet, clearEverything);
 }
 
-function createButtons(value, element)
+function createButtons(value, element, func)
 {
     let newBtn = document.createElement("button");
     newBtn.textContent = value;
+    newBtn.onclick = () => func(value);
     element.appendChild(newBtn);
+}
+
+function numberPressed(value)
+{
+    inputString.textContent += value;
+}
+
+function operatorPressed(value)
+{
+    
+}
+
+function equalPressed()
+{
+
+}
+
+function decimalPressed(value)
+{
+    
+}
+
+function clearEverything(value)
+{
+    inputString.textContent = '';
+    resultString.textContent = '';
 }
