803ab70ec23e05c6936e4bd9afc2ed2d44904fec
Jordancojp <117468147+Jordancojp@users.noreply.github.com>
2024-10-29 19:35:43 +0800

    Fixed the layout of numpad by splitting into 3 sections, top row = operators, numpad = numbers, and equals sign

--- javascript.js
+++ javascript.js
@@ -1,19 +1,20 @@
-var btnTopRow = ['/','x','-','+'];
-var btnOptions = ['7','8','9','4','5','6','1','2','3','0','.','CE'];
-var btnArr = document.querySelector("#BtnArr");
+var topRowValues = ['/','x','-','+'];
+var numPadValues = ['7','8','9','4','5','6','1','2','3','0','.','CE'];
+var topRowElement = document.querySelector("#TopRow");
+var numPadElemenet = document.querySelector("#NumPad");
 
 generateNumPad();
 
 function generateNumPad()
 {
     let iterator = 0;
-    btnTopRow.forEach(value => createButtons(value));
-    btnOptions.forEach(value => createButtons(value));
+    topRowValues.forEach(value => createButtons(value, topRowElement));
+    numPadValues.forEach(value => createButtons(value, numPadElemenet));
 }
 
-function createButtons(value)
+function createButtons(value, element)
 {
     let newBtn = document.createElement("button");
     newBtn.textContent = value;
-    btnArr.appendChild(newBtn);
+    element.appendChild(newBtn);
 }
